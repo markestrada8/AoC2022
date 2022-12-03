cc1e01234b969a7301c1ba6ff17fe15d0d22652b
Mark Estrada <mark.estrada.dev>
2022-12-03 17:02:18 -0600

    day two

---
+++ day_2/part_one.py
@@ -0,0 +1,21 @@
+outcomes = {
+    'A': {
+        'X': 4,
+        'Y': 8,
+        'Z': 3
+    },
+    'B': {
+        'X': 1,
+        'Y': 5,
+        'Z': 9
+    },
+    'C': {
+        'X': 7,
+        'Y': 2,
+        'Z': 6
+    },
+}
+
+with open('data.txt', 'r') as file:
+    print(sum([outcomes[line.split()[0]][line.split()[1]]
+          for line in file.readlines()]))
